7935daf7248e68e4d7dcdcfd555696741f94a2fd
Joey Freeland <joey@free.land>
2023-04-13 00:14:43 -0400

    chore: lazy

--- .config/nvim-lua-user/plugins/for-me.lua
+++ .config/nvim-lua-user/plugins/for-me.lua
@@ -1,26 +1,40 @@
+-- This is probably stuff that I could get out of the community plugins but
+-- I've grown accustomed to having it around.
 return {
   {
     "psliwka/vim-smoothie",
+    lazy = false,
+    config = function() end,
     opts = {},
   },
   {
     "wakatime/vim-wakatime",
+    lazy = false,
+    config = function() end,
     opts = {},
   },
   {
    "bronson/vim-trailing-whitespace",
+    lazy = false,
+    config = function() end,
     opts = {},
   },
   {
     "tpope/vim-sleuth",
+    lazy = false,
+    config = function() end,
     opts = {},
   },
   {
     "fatih/vim-go",
+    lazy = false,
+    config = function() end,
     opts = {},
   },
   {
     "mrjones2014/smart-splits.nvim",
+    lazy = false,
+    config = function() end,
     opts = {},
   }
 }
